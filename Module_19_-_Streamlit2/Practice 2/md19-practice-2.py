19f2e8b0077c743eb07899681c18346fe0c7c522
raafarosa <141688193+raafarosa@users.noreply.github.com>
2024-03-15 19:48:58 -0300

    Final

--- Module_19_-_Streamlit2/Practice 2/md19-practice-2.py
+++ Module_19_-_Streamlit2/Practice 2/md19-practice-2.py
@@ -124,11 +124,59 @@ def main():
                 step=1,
             )
 
-            # PROFISSÕES
+# PROFISSÕES
             jobs_list = bank["job"].unique().tolist()
             jobs_list.append("all")
             jobs_selected = st.multiselect(
                 label="Profissões:", options=jobs_list, default=["all"]
+            )
+
+            # ESTADO CIVIL
+            marital_list = bank["marital"].unique().tolist()
+            marital_list.append("all")
+            marital_selected = st.multiselect("Estado Civil:", marital_list, ["all"])
+
+            # DEFAULT
+            default_list = bank["default"].unique().tolist()
+            default_list.append("all")
+            default_selected = st.multiselect("Default:", default_list, ["all"])
+
+            # FINANCIAMENTO
+            housing_list = bank["housing"].unique().tolist()
+            housing_list.append("all")
+            housing_selected = st.multiselect(
+                "Tem financiamento imobiliário?", housing_list, ["all"]
+            )
+
+            # EMPRÉSTIMO
+            loan_list = bank["loan"].unique().tolist()
+            loan_list.append("all")
+            loan_selected = st.multiselect("Tem empréstimo?", loan_list, ["all"])
+
+            # CONTATO
+            contact_list = bank["contact"].unique().tolist()
+            contact_list.append("all")
+            contact_selected = st.multiselect("Meio de contato:", contact_list, ["all"])
+
+            # MÊS DO CONTATO
+            month_list = bank["month"].unique().tolist()
+            month_list.append("all")
+            month_selected = st.multiselect("Mês do contato:", month_list, ["all"])
+
+            # DIA DA SEMANA
+            day_of_week_list = bank["day_of_week"].unique().tolist()
+            day_of_week_list.append("all")
+            day_of_week_selected = st.multiselect(
+                "Dia da semana do contato:", day_of_week_list, ["all"]
+            )
+
+            bank = (
+                bank.query("age >= @idades[0] and age <= @idades[1]")
+                .pipe(multiselect_filter, "job", jobs_selected)
+                .pipe(multiselect_filter, "marital", marital_selected)
+                .pipe(multiselect_filter, "default", default_selected)
+                .pipe(multiselect_filter, "housing", housing_selected)
+                .pipe(multiselect_filter, "loan", loan_selected)
                 .pipe(multiselect_filter, "contact", contact_selected)
                 .pipe(multiselect_filter, "month", month_selected)
                 .pipe(multiselect_filter, "day_of_week", day_of_week_selected)
