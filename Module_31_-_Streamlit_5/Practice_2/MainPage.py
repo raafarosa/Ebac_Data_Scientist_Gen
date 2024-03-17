bdb0ad2c67688db5212bec9cdfa38ec72f83d0ff
raafarosa <rra2308br@gmail.com>
2024-03-17 01:45:31 -0300

    Update

--- Module_31_-_Streamlit_5/Practice_2/MainPage.py
+++ Module_31_-_Streamlit_5/Practice_2/MainPage.py
@@ -72,10 +72,6 @@ def main():
 
 
     st.sidebar.markdown('''
-                        <div style="text-align:center">
-                            <img src="https://raw.githubusercontent.com/raafarosa/Ebac_Data_Scientist_General/main/utilities/newebac_logo_black_half.png alt="ebac-logo" width=50%>
-                        </div>
-
                         # **Profissão: Cientista de Dados**
                         ### **Projeto de Agrupamento Hierárquico**
 
