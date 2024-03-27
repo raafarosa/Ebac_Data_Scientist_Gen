2ecbbf5abf1bf76f79bd71e409d233722c693793
raafarosa <141688193+raafarosa@users.noreply.github.com>
2024-03-27 20:17:00 -0300

    Updates

--- Module_38_Streamlit_FinalProject/Final_Project/MainPage_Pycaret.py
+++ Module_38_Streamlit_FinalProject/Final_Project/MainPage_Pycaret.py
@@ -1,30 +1,27 @@
 # Imports
-import pandas            as pd
-import streamlit         as st
-
-from io                     import BytesIO
+import pandas as pd
+import streamlit as st
+import xlsxwriter
+from io import BytesIO
 from pycaret.classification import load_model, predict_model
 
-
-@st.cache
+@st.cache_data
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
 # Função para converter o df para excel
-@st.cache
+@st.cache_data
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.save()
     processed_data = output.getvalue()
     return processed_data
-
 
 # Função principal da aplicação
 def main():
     # Configuração inicial da página da aplicação
-    st.set_page_config(page_title = 'PyCaret', \
+    st.set_page_config(page_title='PyCaret', \
         layout="wide",
         initial_sidebar_state='expanded'
     )
@@ -35,7 +32,7 @@ def main():
     
     # Botão para carregar arquivo na aplicação
     st.sidebar.write("## Suba o arquivo")
-    data_file_1 = st.sidebar.file_uploader("Bank Credit Dataset", type = ['csv','ftr'])
+    data_file_1 = st.sidebar.file_uploader("Bank Credit Dataset", type=['csv', 'ftr'])
 
     # Verifica se há conteúdo carregado na aplicação
     if (data_file_1 is not None):
@@ -47,9 +44,8 @@ def main():
 
         df_xlsx = to_excel(predict)
         st.download_button(label='📥 Download',
-                            data=df_xlsx ,
-                            file_name= 'predict.xlsx')
-
+                           data=df_xlsx,
+                           file_name='predict.xls')
 
 if __name__ == '__main__':
-	main()
+    main()
